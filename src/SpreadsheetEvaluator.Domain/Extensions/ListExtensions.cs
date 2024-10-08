9703fef36db6741b0e759a91dce9b982c9362bc1
GintasS <svedgintas@gmail.com>
2024-10-07 21:10:23 -0700

    Extension methods

---
+++ src/SpreadsheetEvaluator.Domain/Extensions/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SpreadsheetEvaluator.Domain.Extensions
+{
+    public static class ListExtensions
+    {
+        public static bool HasMismatchingElementTypes<T>(this List<T> list) where T : struct
+        {
+            if (list == null || list.Count <= 1)
+            {
+                return false;
+            }
+
+            return list.Distinct().Count() != 1;
+        }
+    }
+}
---
+++ src/SpreadsheetEvaluator.Domain/Extensions/ObjectExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using SpreadsheetEvaluator.Domain.Models.MathModels;
+
+namespace SpreadsheetEvaluator.Domain.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNumber(this object value)
+        {
+            return value is sbyte
+                    || value is byte
+                    || value is short
+                    || value is ushort
+                    || value is int
+                    || value is uint
+                    || value is long
+                    || value is ulong
+                    || value is float
+                    || value is double
+                    || value is decimal;
+        }
+
+        public static bool IsValidCellValue(this object value)
+        {
+            return value.IsNumber()
+                   || value is string
+                   || value is bool
+                   || value is Formula
+                   || value is CellValue;
+        }
+
+        public static object CalculateMathExpression(this object valueToCompute)
+        {
+            object returnValue;
+            try
+            {
+                returnValue = new DataTable().Compute(valueToCompute.ToString(), null);
+            }
+            catch
+            {
+                returnValue = null;
+            }
+
+            return returnValue;
+        }
+    }
+}
