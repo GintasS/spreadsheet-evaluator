ec14ac29750aca28752e7287cba98292f6e0f103
GintasS <svedgintas@gmail.com>
2024-10-07 21:08:25 -0700

    Added request and responses.

---
+++ src/SpreadsheetEvaluator.Domain/Models/Requests/JobsPostRequest.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using SpreadsheetEvaluator.Domain.Models.Enums;
+using SpreadsheetEvaluator.Domain.Models.MathModels;
+
+namespace SpreadsheetEvaluator.Domain.Models.Requests
+{
+    public class JobsPostRequest
+    {
+        [JsonProperty("email")]
+        [JsonRequired]
+        public string Email { get; set; }
+
+        [JsonProperty("results")]
+        [JsonRequired]
+        public List<JobsPostModel> Jobs { get; set; }
+    }
+
+    public class JobsPostModel
+    {
+        [JsonProperty("id")]
+        [JsonRequired]
+        public string Id { get; set; }
+
+        [JsonProperty("data")]
+        [JsonRequired]
+        public List<List<JobsPostValueModel>> Values { get; set; }
+    }
+
+    public class JobsPostValueModel
+    {
+        [JsonProperty("value")]
+        public Values Values { get; set; }
+
+        [JsonProperty("error")]
+        public string Error { get; set; }
+
+        public JobsPostValueModel(CellValue cellValue)
+        {
+            Values = new Values();
+
+            switch (cellValue.CellType)
+            {
+                case CellType.Number:
+                    Values.Number = (decimal)cellValue.Value;
+                    break;
+                case CellType.Text:
+                    Values.Text = cellValue.Value.ToString();
+                    break;
+                case CellType.Boolean:
+                    Values.Boolean = (bool)cellValue.Value;
+                    break;
+                case CellType.Error:
+                    Error = cellValue.Value.ToString();
+                    Values = null;
+                    break;
+            }
+        }
+    }
+
+    public class Values
+    {
+        [JsonProperty("boolean")]
+        public bool? Boolean { get; set; }
+        [JsonProperty("number")]
+        public decimal? Number { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+}
---
+++ src/SpreadsheetEvaluator.Domain/Models/Responses/JobsGetRawResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace SpreadsheetEvaluator.Domain.Models.Responses
+{
+    public class JobsGetRawResponse
+    {
+        public string SubmissionUrl { get; set; }
+        [JsonExtensionData]
+        public Dictionary<string, JToken> Jobs { get; set; }
+    }
+}
